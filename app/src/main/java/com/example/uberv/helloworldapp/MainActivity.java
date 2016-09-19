ce1280afb155c0ca5969b16fb5383c67a4fe6c41
Rikharthu <uberviolence@gmail.com>
2016-09-19 20:55:51 +0300

    Initial commit

---
+++ app/src/main/java/com/example/uberv/helloworldapp/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.uberv.helloworldapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.seekBar)
+    SeekBar mSeekBar;
+    @BindView(R.id.textView)
+    TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        updateTextViewText(mSeekBar.getProgress());
+
+        mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                updateTextViewText(seekBar.getProgress());
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    private void updateTextViewText(int progress){
+        String content = "Hello World!";
+        int length = content.length();
+        double count = length/100.0*progress;
+        Log.d("rounded",(int)Math.floor(count)+"");
+        mTextView.setText(content.substring(0,(int)Math.floor(count)));
+    }
+
+}
